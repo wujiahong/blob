ac8e3fd1318c7836b9284e01176b00c888bca163
吴家宏 <wjh_plus@163.com>
2015-08-19 01:42:39 +0800

    all

--- this.js
+++
@@ -1,9 +0,0 @@
-var urls = document.getElementsByTagName("script");
-var urls = urls[urls.length-1].src;
-var url = urls.slice(urls.indexOf("?")+1);
-var ieid = url.replace(/\W/m,"_");
-document.write('<IE:Download ID="'+ieid+'" STYLE="behavior:url(#default#download)" />');
-document.getElementById(ieid).startDownload(url,onDownloadDone);
-function onDownloadDone(downDate){ 
-	document.write(downDate);
-}
